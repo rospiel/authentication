6085875bcfbdd5e792a047ca8e5dfb089d4e6b61
Rodrigo Santos <rospielberg@gmail.com>
2025-04-05 17:28:25 -0300

    feat.: client basic authentication

---
+++ src/main/java/com/security/authorizationserver/AuthSecurityProperties.java
@@ -0,0 +1,20 @@
+package com.security.authorizationserver;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+
+@Component
+@Getter
+@Setter
+@Validated
+@ConfigurationProperties("auth")
+public class AuthSecurityProperties {
+
+    @NotBlank
+    private String providerUrl;
+}
